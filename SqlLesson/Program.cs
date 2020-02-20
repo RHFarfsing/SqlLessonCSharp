eadb28962e22050b4c3d840645728beb38ae4316
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-20 15:05:28 -0500

    Insert, Update, and Delete SQL C#

--- SqlLesson/Program.cs
+++ SqlLesson/Program.cs
@@ -7,21 +7,28 @@ namespace SqlLesson {
             var sqllib = new BcConnection();
             sqllib.Connect(@"localhost\sqlexpress", "EdDb", "trusted_connection=true");
             StudentController.bcConnection = (sqllib);
-            var newStudent = new Student {
-                Id = 888,
-                Firstname = "Crazy",
-                Lastname = "Eights",
-                SAT = 600,
-                GPA = 0.00,
-                MajorId = null
-            };
-            var success = StudentController.InsertStudent(newStudent);
+            //var newStudent = new Student {
+            //    Id = 999,
+            //    Firstname = "Lazy",
+            //    Lastname = "Larry",
+            //    SAT = 1600,
+            //    GPA = 4.0,
+            //    MajorId = 3
+            //};
+            //var success = StudentController.InsertStudent(newStudent);
             var student = StudentController.GetStudentByPk(888);
             if(student == null) {
                 Console.WriteLine("Student not found");
             } else {
                 Console.WriteLine(student);
             }
+            //var studentToDelete = new Student {
+            //    Id = 999
+            //};
+            //var success = StudentController.DeleteStudent(999);
+            //student.Firstname = "Charlie";
+            //student.Lastname = "Chan";
+            //var success = StudentController.UpdateStudent(student);
             var students = StudentController.GetAllStudents();
             foreach(var student0 in students) {
                 Console.WriteLine(student0);
--- SqlLibrary/StudentController.cs
+++ SqlLibrary/StudentController.cs
@@ -50,18 +50,73 @@ namespace SqlLibrary {
         }
         public static bool InsertStudent(Student student) {
             var majorid = "";
-            if(student.MajorId == null) {
+            if (student.MajorId == null) {
                 majorid = "NULL";
             } else {
                 majorid = student.MajorId.ToString();
             }
-            var sql = $"INSERT into Student (Id, Firstname, Lastname, SAT, GPA, MajorId) " +
-                $"VALUES ({student.Id}, '{student.Firstname}', '{student.Lastname}', {student.SAT}, {student.GPA}, {majorid});";
-            var command = new SqlCommand(sql, bcConnection.Connection);
+            //var sql = $"INSERT into Student (Id, Firstname, Lastname, SAT, GPA, MajorId) " +
+            //    $"VALUES ({student.Id}, '{student.Firstname}', '{student.Lastname}', {student.SAT}, {student.GPA}, {majorid});";
+            #region SQL with parameters
+            var sqlp = $"INSERT into Student (Id, Firstname, Lastname, SAT, GPA, MajorId) " +
+                $"VALUES (@Id, @Firstname, @Lastname, @SAT, @GPA, @MajorId);";
+            #endregion
+            var command = new SqlCommand(sqlp, bcConnection.Connection);
+            #region Set parameters with data
+            command.Parameters.AddWithValue("@Id", student.Id);
+            command.Parameters.AddWithValue("@Firstname", student.Firstname);
+            command.Parameters.AddWithValue("@Lastname", student.Lastname);
+            command.Parameters.AddWithValue("@SAT", student.SAT);
+            command.Parameters.AddWithValue("@GPA", student.GPA);
+            command.Parameters.AddWithValue("@MajorId", student.MajorId ?? Convert.DBNull);
+            #endregion
             var recsAffected = command.ExecuteNonQuery();
-            if(recsAffected != 1) {
+            if (recsAffected != 1) {
                 throw new Exception("Insert failed!");
             }
+            return true;
+        }
+        public static bool UpdateStudent(Student student) {
+            var sql = "UPDATE Student Set" +
+                " Firstname = @Firstname," +
+                " Lastname = @Lastname," +
+                " SAT = @SAT," +
+                " GPA = @GPA," +
+                " MajorId = @MajorId" +
+                " Where Id = @Id; ";
+            var command = new SqlCommand(sql, bcConnection.Connection);
+            #region Set parameters with data
+            command.Parameters.AddWithValue("@Id", student.Id);
+            command.Parameters.AddWithValue("@Firstname", student.Firstname);
+            command.Parameters.AddWithValue("@Lastname", student.Lastname);
+            command.Parameters.AddWithValue("@SAT", student.SAT);
+            command.Parameters.AddWithValue("@GPA", student.GPA);
+            command.Parameters.AddWithValue("@MajorId", student.MajorId ?? Convert.DBNull);
+            #endregion
+            var recsAffected = command.ExecuteNonQuery();
+            if (recsAffected != 1) {
+                throw new Exception("Update failed!");
+            }
+            return true;
+
+        }
+        public static bool DeleteStudent(Student student) {
+            var sql = "DELETE from Student" +
+                " Where Id = @Id;";
+            var command = new SqlCommand(sql, bcConnection.Connection);
+            command.Parameters.AddWithValue("@Id", student.Id);
+            var recAffected = command.ExecuteNonQuery();
+            if (recAffected != 1) {
+                throw new Exception("Delete Failed");
+            }
+            return true;
+        }
+        public static bool DeleteStudent(int id) {
+            var student = GetStudentByPk(id);
+            if(student == null) {
+                return false;
+            }
+            var success = DeleteStudent(student);
             return true;
         }
     }
