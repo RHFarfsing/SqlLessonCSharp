784830dd0979a2fd841fb79d4164bc5aeaccb819
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-20 09:28:58 -0500

    Add project files.

---
+++ SqlLesson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SqlLibrary;
+
+namespace SqlLesson {
+    class Program {
+       static void Main(string[] args) {
+            var sqllib = new BcConnection();
+            sqllib.Connect(@"localhost\sqlexpress", "EdDb", "trusted_connection=true");
+            Student.bcConnection = (sqllib);
+            var students = Student.GetAllStudents();
+            sqllib.Disconnect();
+        }
+    }
+}
